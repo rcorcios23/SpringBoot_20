4e1c956ee71240724caa7ecd35df1180e62791e1
rcorcios23 <r.corcios@gmail.com>
2017-08-21 13:15:57 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+
+
+
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll().permitAll()
+                .and()
+                .httpBasic();
+        http
+                .csrf().disable();
+        http
+                .headers().frameOptions().disable();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        auth
+                .userDetailsService(userDetailsServiceBean());
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
